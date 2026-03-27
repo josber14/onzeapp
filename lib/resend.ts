0b1077a3025cb0d302607acee74a36b8dbd89c0c
Josber Marcano <josbermarcano@MacBook-Air-de-Josber.local>
2026-03-27 18:41:32 -0300

    Integrate Resend for password reset emails

---
+++ lib/resend.ts
@@ -0,0 +1,7 @@
+import { Resend } from "resend";
+
+if (!process.env.RESEND_API_KEY) {
+  throw new Error("Falta RESEND_API_KEY en .env.local");
+}
+
+export const resend = new Resend(process.env.RESEND_API_KEY);
